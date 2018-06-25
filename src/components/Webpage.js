922365a48be6aafd452103233336032f6cebc93a
munirkizilcali <munir.kizilcali@gmail.com>
2018-06-25 09:15:37 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FillerText from './FillerText'
+
+class Webpage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h3>The world's coolest webpage</h3>
+				<FillerText />
+				<FillerText />
+			</div>
+			)
+	}
+}
+
+export default Webpage;
